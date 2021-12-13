249f47280d9eeda40d62939d897d9446b89f06cd
felix yangsen <felixyangsen96@gmail.com>
2021-12-13 20:38:28 +0700

    create jwt otken

--- project-2/config/token.go
+++ project-2/config/token.go
@@ -1,1 +1,29 @@
 package config
+
+import (
+	"github.com/golang-jwt/jwt"
+)
+
+type MyClaim struct {
+	jwt.StandardClaims
+	Username string
+}
+
+var mySigningKey = []byte("MySecrets")
+
+func CreateToken(username string) string {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, MyClaim{
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: 15000,
+			Issuer:    "project-2",
+		},
+		Username: username,
+	})
+
+	signedStr, err := token.SignedString(mySigningKey)
+	if err != nil {
+		panic(err)
+	}
+
+	return signedStr
+}
--- project-2/go.mod
+++ project-2/go.mod
@@ -3,8 +3,9 @@ module project-2
 go 1.16
 
 require (
-	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
-	github.com/gorilla/mux v1.8.0 // indirect
-	gorm.io/driver/postgres v1.2.3 // indirect
-	gorm.io/gorm v1.22.4 // indirect
+	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d
+	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/gorilla/mux v1.8.0
+	gorm.io/driver/postgres v1.2.3
+	gorm.io/gorm v1.22.4
 )
