ff6ec9b89bca8f3f12b71edf4abdcc83e272d629
felixyangsen <felixyangsen96@gmail.com>
2022-02-08 09:39:07 +0700

    minor changes in json tag

--- project-4/model/transactionHistoryModel.go
+++ project-4/model/transactionHistoryModel.go
@@ -3,11 +3,11 @@ package model
 import "time"
 
 type TransactionHistory struct {
-	ID         int       `json:"id"`
-	ProductID  int       `json:"product_id"`
-	UserID     int       `json:"user_id"`
-	Quantity   int       `json:"quantity" binding:"required"`
-	TotalPrice int       `json:"total_price" binding:""required`
-	CreatedAt  time.Time `json:"created_at"`
+	ID         int        `json:"id"`
+	ProductID  int        `json:"product_id"`
+	UserID     int        `json:"user_id"`
+	Quantity   int        `json:"quantity" binding:"required"`
+	TotalPrice int        `json:"total_price" binding:"required"`
+	CreatedAt  time.Time  `json:"created_at"`
 	UpdatedAt  *time.Time `json:"updated_at"`
 }
